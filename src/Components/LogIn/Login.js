dc9c3b407ae8fb2b306a7685482dbafa4d8487b5
Jack Lansing <jack.lansing@live.com>
2020-04-13 15:10:28 -0400

    updated login form to use username field

--- src/Components/LogIn/Login.js
+++ src/Components/LogIn/Login.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <>
@@ -9,9 +13,9 @@ class Login extends Component {
         </header>
         <section className="Sign-Up">
           <h2>Log In</h2>
-          <form className="Sign-Up__form">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
+          <form className="Sign-Up__form" onSubmit={this.handleSubmit}>
+            <label htmlFor="user_name">User Name</label>
+            <input type="user_name" id="user_name" name="user_name" />
             <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password" />
             <input type="submit" />
