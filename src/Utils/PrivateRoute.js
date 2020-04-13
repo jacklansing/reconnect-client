a19f3a644d3c24260d129fc9ae11de487e9d8926
Jack Lansing <jack.lansing@live.com>
2020-04-13 15:33:29 -0400

    added Routes for managing public / private or logged out / in

---
+++ src/Utils/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import TokenService from '../services/token-service';
+
+const PrivateRoute = ({ component, ...props }) => {
+  const Component = component;
+  return (
+    <Route
+      {...props}
+      render={componentProps =>
+        TokenService.hasAuthToken() ? (
+          <Component {...componentProps} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
