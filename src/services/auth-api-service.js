02e146d35667975ce8a8ce74f5d277fc32b21cb8
Jack Lansing <jack.lansing@live.com>
2020-04-13 14:18:23 -0400

    ran prettier format

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -8,10 +8,8 @@ const AuthApiService = {
         'content-type': 'application/json'
       },
       body: JSON.stringify(user)
-    }).then(res => 
-      (!res.ok)
-      ? res.json().then(e => Promise.reject(e))
-      : res.json()
-      )
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
   }
 };
