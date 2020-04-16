55361cd0d83a62c4017879b498167d8d7d31bbea
Jack Lansing <jack.lansing@live.com>
2020-04-16 17:39:10 -0400

    config fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  // API_ENDPOINT: 'https://reconnect-tech.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://reconnect-tech.herokuapp.com/api',
   TOKEN_KEY: 'reconnect-authentication-token'
 };
