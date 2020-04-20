cd790f4d75258a0ff215116f5284af8df8d1f26c
Jack Lansing <jack.lansing@live.com>
2020-04-20 11:33:44 -0400

    basic toggle nav working

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,9 +3,6 @@ import './Landing.css';
 const Landing = props => {
   return (
     <>
-      <header role="banner" className="Hero">
-        <h1>Re-Connect</h1>
-      </header>
       <section className="Info">
         <p>
           Got a bunch of old iPhones sitting in your drawer? Use Re-Connect to
