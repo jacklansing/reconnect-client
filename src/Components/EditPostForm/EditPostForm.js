ce2e602f56419663763767b48d505d1f206a6bb3
Jack Lansing <jack.lansing@live.com>
2020-04-20 21:47:37 -0400

    refactor EditPostForm and NewPostForm to use Composition from PostForm, more consistent!

--- src/Components/EditPostForm/EditPostForm.js
+++ src/Components/EditPostForm/EditPostForm.js
@@ -1,105 +1,17 @@
 import React, { Component } from 'react';
-import { Button, Textarea, Input, Select, Label } from '../Utils/Utils';
+import PostForm from '../PostForm/PostForm';
 import AuthApiService from '../../services/auth-api-service';
-import './EditPostForm.css';
 
 class EditPostForm extends Component {
-  state = {
-    error: null,
-    title: '',
-    description: '',
-    device: '',
-    condition: '',
-    location: ''
-  };
-
-  componentDidMount() {
-    this.setState({
-      ...this.props.location.postProps
-    });
-  }
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const updatedDevice = {
-      id: this.state.post_id,
-      title: this.state.title,
-      description: this.state.description,
-      device: this.state.device,
-      condition: this.state.condition,
-      location: this.state.location
-    };
-    try {
-      await AuthApiService.updateDevice(updatedDevice);
-      this.props.history.push('/my-posts');
-    } catch (e) {
-      this.setState({ error: e });
-    }
-  };
-
   render() {
     return (
-      <>
-        <section className="Edit-Post">
-          <h2>Update Post</h2>
-          <form className="Edit-Post__form" onSubmit={this.handleSubmit}>
-            <Label htmlFor="title">Title</Label>
-            <Input
-              type="text"
-              id="title"
-              name="title"
-              value={this.state.title}
-              onChange={e => this.setState({ title: e.target.value })}
-            />
-            <Label htmlFor="description">Description</Label>
-            <Textarea
-              id="description"
-              name="description"
-              value={this.state.description}
-              onChange={e => this.setState({ description: e.target.value })}
-            />
-            <Label htmlFor="device">Device Type</Label>
-            <Select
-              name="device"
-              id="device"
-              value={this.state.device}
-              onChange={e => this.setState({ device: e.target.value })}
-            >
-              <option value="Android" default>
-                Android
-              </option>
-              <option value="iPhone">iPhone</option>
-              <option value="Windows">Windows</option>
-              <option value="Macbook">Macbook</option>
-            </Select>
-            <Label htmlFor="condition">Condition</Label>
-            <Select
-              name="condition"
-              id="condition"
-              value={this.state.condition}
-              onChange={e => this.setState({ condition: e.target.value })}
-            >
-              <option value="very good">Very Good</option>
-              <option value="good" default>
-                Good
-              </option>
-              <option value="okay">Okay</option>
-              <option value="damaged">Damaged</option>
-            </Select>
-            <Label htmlFor="location">Location</Label>
-            <Select
-              id="location"
-              name="location"
-              value={this.state.location}
-              onChange={e => this.setState({ location: e.target.value })}
-            >
-              <option value="Albany, NY">Albany, NY</option>
-              <option value="Schenectady, NY">Schenectady, NY</option>
-            </Select>
-            <Button type="submit">Update Post</Button>
-          </form>
-        </section>
-      </>
+      <PostForm
+        location={this.props.location}
+        history={this.props.history}
+        onSubmit={AuthApiService.updateDevice}
+        redirectAfterSubmit={'/my-posts'}
+        buttonText={'Update Post'}
+      />
     );
   }
 }
--- src/Components/NewPostForm/NewPostForm.js
+++ src/Components/NewPostForm/NewPostForm.js
@@ -1,75 +1,17 @@
 import React, { Component } from 'react';
-import { Button, Textarea, Input, Select, Label } from '../Utils/Utils';
+import PostForm from '../PostForm/PostForm';
 import AuthApiService from '../../services/auth-api-service';
-import './NewPostForm.css';
 
 class NewPostForm extends Component {
-  state = {
-    error: null
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const { title, description, device, condition, location } = e.target;
-
-    AuthApiService.postDevice({
-      title: title.value,
-      description: description.value,
-      device: device.value,
-      condition: condition.value,
-      location: location.value
-    })
-      .then(device => {
-        title.value = '';
-        description.value = '';
-        device.value = '';
-        condition.value = '';
-        location.value = '';
-      })
-      .catch(res => {
-        this.setState({ error: res.error });
-      });
-    // TODO: send to recent messages screen once implemented
-    this.props.history.push('/');
-  };
-
   render() {
     return (
-      <>
-        <section className="New-Post">
-          <h2>New Post</h2>
-          <form className="New-Post__form" onSubmit={this.handleSubmit}>
-            <Label htmlFor="title">Title</Label>
-            <Input type="text" id="title" name="title" />
-            <Label htmlFor="description">Description</Label>
-            <Textarea id="description" name="description"></Textarea>
-            <Label htmlFor="device">Device Type</Label>
-            <Select name="device" id="device">
-              <option value="Android" default>
-                Android
-              </option>
-              <option value="iPhone">iPhone</option>
-              <option value="Windows">Windows</option>
-              <option value="Macbook">Macbook</option>
-            </Select>
-            <Label htmlFor="condition">Condition</Label>
-            <Select name="condition" id="condition">
-              <option value="very good">Very Good</option>
-              <option value="good" default>
-                Good
-              </option>
-              <option value="okay">Okay</option>
-              <option value="damaged">Damaged</option>
-            </Select>
-            <Label htmlFor="location">Location</Label>
-            <Select id="location" name="location">
-              <option value="Albany, NY">Albany, NY</option>
-              <option value="Schenectady, NY">Schenectady, NY</option>
-            </Select>
-            <Button type="submit">New Post</Button>
-          </form>
-        </section>
-      </>
+      <PostForm
+        location={this.props.location}
+        history={this.props.history}
+        onSubmit={AuthApiService.postDevice}
+        redirectAfterSubmit={'/my-posts'}
+        buttonText={'New Post'}
+      />
     );
   }
 }
